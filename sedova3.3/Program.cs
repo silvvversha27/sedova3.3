551dec0cf367bf79d0b32a64876996b35ece0f1b
Анна <@DASHA>
2021-11-24 16:14:05 +0200

    Добавьте файлы проекта.

---
+++ sedova3.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace sedova3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                double S = 0;
+                for (int i = 0, h = 2; i < n; i++, h += 2)
+                {
+                    long F = 1;
+                    for (int j = 1; j <= h; j++) F *= j;
+                    if (i % 2 == 0) S += -Math.Pow(2 * x, h) / F;
+                    else S += Math.Pow(2 * x, h) / F;
+                }
+                Console.WriteLine("S={0:f2}", S);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
